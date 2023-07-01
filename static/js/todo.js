1a0291db015e3f0e9d616b5e61f0e9ddac66407a
eunbum kim <icesnake72@gmail.com>
2023-07-01 13:27:34 +0900

    todo_done_update 기능 추가중

--- static/js/todo.js
+++ static/js/todo.js
@@ -33,3 +33,19 @@ function todoDelete()
 {
     console.log("삭제")
 }
+
+function checkBoxClick(id, user_id)
+{
+    var url = "http://127.0.0.1:5001/";
+    var data = {todo_id:id, user_id:user_id};
+
+    console.log(data);
+
+    // XMLHttpRequest 객체 생성
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    // 요청 보내기
+    xhr.send(JSON.stringify(data));
+}
